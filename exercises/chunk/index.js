eeaf748b9cb646853ae45f12d34ce5e14ba908b0
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-05 22:42:44 -0300

    other way create array chunck

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,24 +8,32 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-  const newArr = []
-  let a = []
-  array.map((item, index) => {
-    a.push(item)
-    if (a.length === size) {
-      newArr.push(a)
-      a = []
-    }
-  })
-  if (array.length % size !== 0) {
-    const extract = array.slice(-Math.abs(array.length % size))
-    newArr.push(extract)
-  }
-  return newArr
-}
+// function chunk(array, size) {
+//   const newArr = []
+//   let a = []
+//   array.map((item, index) => {
+//     a.push(item)
+//     if (a.length === size) {
+//       newArr.push(a)
+//       a = []
+//     }
+//   })
+//   if (array.length % size !== 0) {
+//     const extract = array.slice(-Math.abs(array.length % size))
+//     newArr.push(extract)
+//   }
+//   return newArr
+// }
 
-const arr = [1, 2, 3, 4, 5, 6, 7]
-console.log(arr.length % 5);
+function chunk(array, size) {
+  const chuncked = []
+  let index = 0
+
+  while (index < array.length) {
+    chuncked.push(array.slice(index, index + size))
+    index += size
+  }
+  return chuncked
+}
 
 module.exports = chunk;
