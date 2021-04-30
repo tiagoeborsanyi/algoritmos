37891a8d58423ebe6bfc9d76ba39481f291f4a2e
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-04-30 17:08:21 -0300

    finish exercise volwels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
+
+vowels('hi there!')
 
 module.exports = vowels;
+
+// function vowels(str) {
+//   const vogal = ['a', 'e', 'i', 'o', 'u']
+//   const result = []
+//   return vogal.map(v => str.toLowerCase().split('').filter(el => el === v)).join().split(',').join('').length
+// }
