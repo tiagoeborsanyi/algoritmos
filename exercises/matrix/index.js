e7637b94195b37dc54fc0ea1ed4a51b0f736bd31
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-03 12:24:40 -0300

    finish exercise matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,69 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = []
+  
+  for (let i = 0; i < n; i++) {
+    results.push([])
+  }
+
+  let counter = 1;
+  let startColumn = 0, endColumn = n - 1, startRow = 0, endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // top Row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter
+      counter++
+    }
+    startRow++
+
+    // Right column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter
+      counter++
+    }
+    endColumn--
+
+    // Bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter
+      counter++
+    }
+    endRow--
+
+    // start Column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter
+      counter++
+    }
+    startColumn++
+  }
+
+  console.log(results)
+  return results
+}
+
+matrix(4)
 
 module.exports = matrix;
+
+// function matrix(n) {
+//   const results = []
+//   let counter = 1, startColumn = 0, endColumn = n, startRow = 0, endRow = n
+
+//   if (startColumn <= endColumn && startRow <= endColumn) {
+//     for (let c = startColumn; c <= endColumn; c++) {
+//       results.push([startRow])[counter]
+//       counter++
+//     }
+//     startRow++
+//     for (let c = startRow; c <= endRow; c++) {
+//       results.push([counter])[endColumn]
+//       counter++
+//     }
+//     endRow--
+//   }
+//   console.log(results)
+// }
