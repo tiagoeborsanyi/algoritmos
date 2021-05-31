c97f15daa997f7779b7abdb777223cc04b39377c
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-05-31 12:46:29 -0300

    finish class Queue on book

--- book/queue/Queue.js
+++ book/queue/Queue.js
@@ -22,12 +22,40 @@ class Queue {
         return result;
     }
     peek() {
-        return 0;
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items[this.lowestCount];
     }
     isEmpty() {
-        return true;
+        return this.size() === 0;
     }
     size() {
-        return 0;
+        return this.count - this.lowestCount;
+    }
+    clear() {
+        this.count = 0,
+            this.lowestCount = 0,
+            this.items = {};
+    }
+    toString() {
+        if (this.isEmpty()) {
+            return '';
+        }
+        let objString = `${this.items[this.lowestCount]}`;
+        for (let i = this.lowestCount + 1; i < this.count; i++) {
+            objString = `${objString}, ${this.items[i]}`;
+        }
+        return objString;
     }
 }
+const queue = new Queue();
+console.log(queue.isEmpty());
+queue.enQueue('jhon');
+queue.enQueue('jack');
+console.log(queue.toString());
+queue.enQueue('Ana');
+console.log(queue.size());
+queue.deQueue();
+queue.deQueue();
+console.log(queue.toString());
--- book/queue/Queue.ts
+++ book/queue/Queue.ts
@@ -1,20 +1,20 @@
 class Queue {
   constructor(
-    public count: number = 0,
-    public lowestCount: number = 0,
-    public items: any = {}
+    private count: number = 0,
+    private lowestCount: number = 0,
+    private items: any = {}
   ) {
     this.count = count,
     this.lowestCount = lowestCount,
     this.items = items
   }
 
-  enQueue(element: number): void {
+  enQueue(element: number | string): void {
     this.items[this.count] = element
     this.count++
   }
 
-  deQueue(): number | undefined {
+  deQueue(): object | undefined {
     if (this.isEmpty()) {
       return undefined
     }
@@ -25,15 +25,47 @@ class Queue {
     return result
   }
 
-  peek(): number {
-    return 0
+  peek(): number | undefined {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    return this.items[this.lowestCount]
   }
 
   isEmpty(): boolean {
-    return true
+    return this.size() === 0
   }
 
   size(): number {
-    return 0
+    return this.count - this.lowestCount
+  }
+
+  clear(): void {
+    this.count = 0,
+    this.lowestCount = 0,
+    this.items = {}
+  }
+
+  toString(): string {
+    if (this.isEmpty()) {
+      return ''
+    }
+
+    let objString: string = `${this.items[this.lowestCount]}`
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`
+    }
+    return objString
   }
 }
+
+const queue = new Queue()
+console.log(queue.isEmpty())
+queue.enQueue('jhon')
+queue.enQueue('jack')
+console.log(queue.toString())
+queue.enQueue('Ana')
+console.log(queue.size())
+queue.deQueue()
+queue.deQueue()
+console.log(queue.toString())
