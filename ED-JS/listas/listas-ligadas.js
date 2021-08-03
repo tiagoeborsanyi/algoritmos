2cdc002cfcbddaac152b763e4abefcad99159817
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-08-02 23:52:02 -0300

    criado o esqueleto de listas ligadas em js

---
+++ ED-JS/listas/listas-ligadas.js
@@ -0,0 +1,54 @@
+function LinkedList() {
+  var Node = function(element) {
+    this.element = element
+    this.next = null
+  }
+
+  var length = 0;
+  var head = null
+
+  // Aficiona um elemento ao final da lista
+  this.append = function(element) {
+
+  }
+
+  // Adiciona um elemento em uma posicao especifica
+  this.insert = function(position, element) {
+
+  }
+
+  // remove o elemento de posicao especifica
+  this.removeAt = function(position) {
+
+  }
+
+  // remove o elemento element
+  this.remove = function(element) {
+
+  }
+
+  // retorna a posicao do elemento
+  this.indexOf = function(element) {
+
+  }
+
+  // retorna se esta vazio ou nao a instancia 
+  this.isEmpty = function() {
+
+  }
+
+  // retorna o tamanho da instancia
+  this.size = function() {
+
+  }
+
+  // converte em string
+  this.toString = function() {
+
+  }
+
+  // imprime no console
+  this.print = function() {
+
+  }
+}
